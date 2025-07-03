3a621d5c3dbc660125dce18bb7fd51bf8e9d895f
NZEFER <zeferovich@mail.ru>
2025-07-03 22:06:45 +0300

    Add files via upload

---
+++ src/test/java/com/example/shoptest/ShoptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
