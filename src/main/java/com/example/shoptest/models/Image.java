73bbb2183bf1b6e74404b72ec2237917506d1874
NZEFER <zeferovich@mail.ru>
2025-07-08 02:08:59 +0300

    add image model

---
+++ src/main/java/com/example/shoptest/models/Image.java
@@ -0,0 +1,40 @@
+package com.example.shoptest.models;
+
+import jakarta.persistence.*;
+import jakarta.security.auth.message.callback.PrivateKeyCallback;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Images")
+@AllArgsConstructor
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "originalFileName")
+    private String originalFileName;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "contextType")
+    private String contentType;
+
+    @Column(name = "isPreviewImage")
+    private boolean isPreviewImage;
+
+    @Lob //@Column(columnDefinition = "LONGBLOB")
+    private byte[] bytes;
+
+    //у одного товара может иметься много фотографий будем делать отношение многие к одному
+    //cascade - что будет со связанным объектом при удалении
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER)  //fetch = FetchType.EAGER способ загрузки, в данном случае подгружается все, есть ленивый метод
+    private Product product;   //преобразуется в fkey
+}
--- src/main/java/com/example/shoptest/models/Product.java
+++ src/main/java/com/example/shoptest/models/Product.java
@@ -4,6 +4,12 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.catalina.LifecycleState;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "products")
@@ -30,4 +36,14 @@ public class Product {
 
     @Column(name = "author")
     private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageId;
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
 }
