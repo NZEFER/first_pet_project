381f3e950783a6395ffadc3ff9d1269eab9147eb
NZEFER <zeferovich@mail.ru>
2025-07-09 16:54:12 +0300

    add user model

---
+++ src/main/java/com/example/shoptest/models/User.java
@@ -0,0 +1,54 @@
+package com.example.shoptest.models;
+
+
+import com.example.shoptest.models.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image avatar;
+
+    @Column(name = "password", length = 24)
+    private String password;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+
+    @Column(name = "dateOfCreated")
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init(){
+        dateOfCreated = LocalDateTime.now();
+    }
+
+}
